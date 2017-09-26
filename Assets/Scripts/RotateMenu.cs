f94daa1eb9e500d4e446f9f11fa83ddebe8c4390
winnie151 <jingwenchenw@gmail.com>
2017-09-25 21:37:48 -0700

    Rotate Menu
    Rotatme Menu is working right now

---
+++ Assets/Scripts/RotateMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RotateMenu : MonoBehaviour
+{
+    void Update()
+    {
+        transform.rotation = Quaternion.LookRotation(Camera.main.transform.forward, Camera.main.transform.up);
+    }
+}
